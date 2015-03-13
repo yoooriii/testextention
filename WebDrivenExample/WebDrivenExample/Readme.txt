7733a8254282e72248f44349ff5160c813f0adfb
Yurii Lokhmatov <zaqqqqqqqq@gmail.com>
2015-03-13 15:38:04 +0200

    adding images and scripts

---
+++ WebDrivenExample/WebDrivenExample/Readme.txt
@@ -0,0 +1,11 @@
+1. The User can (scenario):
+launch the app and WebKit loads the home screen from the server
+fill the web form out with their:
+name
+address
+photo
+preview the result label
+save the result label to server
+print the result label
+check the printer status (offline/door open/incompatible cartridge)
+2. Apple WebKit framework
--- WebDrivenExample/WebDrivenExample/ViewController.m
+++ WebDrivenExample/WebDrivenExample/ViewController.m
@@ -9,14 +9,19 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-<UIWebViewDelegate>
+<UIWebViewDelegate, UIAlertViewDelegate>
 @property (nonatomic, retain) UIWebView		*webView;
+@property (nonatomic, retain) NSArray		*allPictures;
+@property (nonatomic, assign) int	picIndex;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
+	
+	self.allPictures = @[@"'cat2.jpg',100,200", @"'cat3.jpg',300,200", @"'cat4.jpg',150,150", @"'cat5.jpg',200,300"];
+	self.picIndex = 0;
 	
 	self.toolbarItems = @[[[UIBarButtonItem alloc] initWithTitle:@"back" style:UIBarButtonItemStylePlain target:self action:@selector(actBack:)], [[UIBarButtonItem alloc] initWithTitle:@"forward" style:UIBarButtonItemStylePlain target:self action:@selector(actForward:)]];
 	
@@ -28,19 +33,23 @@
 - (void)viewDidAppear:(BOOL)animated {
 	[super viewDidAppear:animated];
 	
-	NSURLRequest *rq = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://sites.google.com/site/crocodilemusher/scriptomatic"]];
+	NSURL *url = [NSURL URLWithString:@"https://sites.google.com/site/crocodilemusher/scriptomatic"];
+	NSString *htmlDir = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"html"];
+	NSString *htmlIndex = [htmlDir stringByAppendingPathComponent:@"index.html"];
+	url = [NSURL fileURLWithPath:htmlIndex];
+	NSURLRequest *rq = [[NSURLRequest alloc] initWithURL:url];
 	[self.webView loadRequest:rq];
 }
 
 #pragma mark - UIWebViewDelegate
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
-	NSLog(@"should?: %@", [request.URL absoluteString]);
 	if ([[request.URL.absoluteString lowercaseString] hasPrefix:@"http://localhost/leitz"]) {
-		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"leitz request:" message:[request.URL absoluteString] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"leitz request:" message:[request.URL absoluteString] delegate:self cancelButtonTitle:@"cancel" otherButtonTitles:@"do smth", nil];
 		[alert show];
 		return NO;
 	}
+	NSLog(@"yes, should: %@", [request.URL absoluteString]);
 	return YES;
 }
 
@@ -50,10 +59,41 @@
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
 	NSLog(@"finish");
+	
+	//	text func
+	NSString *jsInject = @"var script = document.createElement('script');"
+	@"script.type = 'text/javascript';"
+	@"script.text = \"function myFunction(argument1) { "
+	@"var field = document.getElementById('field_3');"
+	@"field.value=argument1;"
+	@"}\";"
+	@"document.getElementsByTagName('head')[0].appendChild(script);";
+	NSString *jsResult = [webView stringByEvaluatingJavaScriptFromString:jsInject];
+	NSLog(@"JS result:\n%@", jsResult);
+	
+	//	image func
+	jsInject = @"var script = document.createElement('script');"
+	@"script.type = 'text/javascript';"
+	@"script.text = \"function myImageFunction(argument1, w, h) { "
+	@"var image = document.getElementById('local_picture');"
+	@"image.src=argument1; image.width=w; image.height=h;"
+	@"}\";"
+	@"document.getElementsByTagName('head')[0].appendChild(script);";
+	jsResult = [webView stringByEvaluatingJavaScriptFromString:jsInject];
+	NSLog(@"JS result:\n%@", jsResult);
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
 	NSLog(@"err:%@", error.localizedDescription);
+}
+
+#pragma mark - UIAlertViewDelegate
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+	if (buttonIndex != alertView.cancelButtonIndex) {
+		NSString *txt = [alertView.message lastPathComponent];
+		[self doSomething:txt];
+	}
 }
 
 #pragma mark -
@@ -64,6 +104,30 @@
 
 - (void)actForward:(id)sender {
 	[self.webView goForward];
+}
+
+- (void)doSomething:(NSString *)text {
+	NSString *script = [NSString stringWithFormat:@"myFunction('%@');", text];//@"document.title";
+
+	NSString *scriptResult = [self.webView stringByEvaluatingJavaScriptFromString:script];
+	NSLog(@"SCRIPT RESULT:{{\n%@\n}};", scriptResult);
+	
+	NSString *argumentStr = self.allPictures[self.picIndex];
+	self.picIndex += 1;
+	if (self.picIndex >= self.allPictures.count) {
+		self.picIndex = 0;
+	}
+	script = [NSString stringWithFormat:@"myImageFunction(%@)", argumentStr];
+	scriptResult = [self.webView stringByEvaluatingJavaScriptFromString:script];
+	NSLog(@"SCRIPT {%@} RESULT:{{\n%@\n}};", script, scriptResult);
+	
+	script = @"document.getElementById('field_name').value;";
+	scriptResult = [self.webView stringByEvaluatingJavaScriptFromString:script];
+	NSLog(@"name:{{%@}};", scriptResult);
+
+	script = @"document.getElementById('field_address').value;";
+	scriptResult = [self.webView stringByEvaluatingJavaScriptFromString:script];
+	NSLog(@"address:{{%@}};", scriptResult);
 }
 
 @end
